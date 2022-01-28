a7845a4801f5f02dbbe5771b4b427808559ee9bc
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-01-28 22:52:52 +0900

    [add]基本構文学習

---
+++ javaWcp/src/javaWcp/Chapter04.java
@@ -0,0 +1,17 @@
+package javaWcp;
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		String name;
+		name = "kaichi";
+		System.out.println(name);
+		
+		name = "hello";
+		System.out.println(name);
+		
+		final int num1;
+		num1 = 7;
+		System.out.println(num1);
+	}
+}
