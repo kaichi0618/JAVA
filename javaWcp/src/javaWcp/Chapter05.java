4cb6a0f8a5b7e7935ece6abee1810e93a53b6e3c
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-01-29 16:50:48 +0900

    [add]プリミティブ型・参照型・ラッパークラス

---
+++ javaWcp/src/javaWcp/Chapter05.java
@@ -0,0 +1,20 @@
+package javaWcp;
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 10L;
+		double double1 = 20.12;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float f1 = 10.12F;
+		Integer int1 = 125;
+		Character c1 = 'A';
+		System.out.println(f1);
+		System.out.println(int1);
+		System.out.println(c1);
+	}
+
+}
