833a9d65c4af045f142caecc9b3d7a27be8b4deb
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-01-29 17:56:42 +0900

    [add]exercise

--- javaWcp/src/javaWcp/Chapter06.java
+++ javaWcp/src/javaWcp/Chapter06.java
@@ -1,5 +1,18 @@
 package javaWcp;
 
 public class Chapter06 {
+	public static void main(String[] args) {
+		int val = 2 + 5;
+		System.out.println(val);
+		val++;
+		System.out.println(val);
+		val *= 5;
+		System.out.println(val);
+		boolean boon = val == 50;
+		System.out.println(boon);
+		boon = (val < 50) && (val % 10 == 0);
+		System.out.println(boon);
+		
+	}
 
 }
