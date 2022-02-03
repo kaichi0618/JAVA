4db049c95348ce4429d5a2536a54a2bde08e6298
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-02-03 20:50:33 +0900

    [add]if break

--- javaWcp/src/javaWcp/Break01.java
+++ javaWcp/src/javaWcp/Break01.java
@@ -1,5 +1,13 @@
 package javaWcp;
 
 public class Break01 {
-
+	public static void main(String[] args) {
+		for (int count = 0; count < 5; count++) {
+			if (count == 2) {
+				break;
+			}
+			System.out.println("for = " + count);
+		}
+		
+	}		
 }
