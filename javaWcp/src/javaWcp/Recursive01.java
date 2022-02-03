0e82c44f9613f6c4b052176c6910817a0b5e2440
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-02-03 23:02:13 +0900

    [add]再帰処理

--- javaWcp/src/javaWcp/Recursive01.java
+++ javaWcp/src/javaWcp/Recursive01.java
@@ -1,5 +1,20 @@
 package javaWcp;
 
 public class Recursive01 {
+	public static void main(String[] args) {
+	    int number = 1;
+//	    printNumというメソッドにnumberという変数を渡している
+	    printNum(number);
+	  }
 
+	  public static int printNum(int number) {
+	    if (number < 50) {
+	      number *= 2;
+	      System.out.println("While01 = " + number);
+	      // 条件にマッチしたら自分自身を再度呼び出す
+//		    printNumというメソッドにnumberという変数を渡している
+	      printNum(number);
+	    }
+	    return number;
+	  }
 }
