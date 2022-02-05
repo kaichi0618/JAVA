df99b9ddbbf2de01f29e42067d9b8d30301c7978
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-02-05 18:13:01 +0900

    [add]human.java

--- javaWcp/src/javaWcp/Human01.java
+++ javaWcp/src/javaWcp/Human01.java
@@ -3,4 +3,13 @@ package javaWcp;
 public class Human01 {
 	public String name;
 	public int age;
+	
+	Human01(){
+		name = "山田";
+		age = 20;
+	}
+	
+	public Human01(String name, int age) {
+		
+	}
 }
